3389abfed71681bf4c14841bf303348ef91f99e6
Anderson Vieira <anderson.si.vieira@gmail.com>
2020-02-13 22:13:44 -0300

    Include the basics for swift

--- basics.playground/Contents.swift
+++ basics.playground/Contents.swift
@@ -1,3 +1,26 @@
 import UIKit
 
-var str = "Hello, playground"
+//MARK: - Strings
+// Print in console
+print("Hi, this is a print")
+
+var name = "Anderson"
+name += " Vieira"
+print(name)
+
+let yearsOld = 35
+print(yearsOld)
+
+//MARK: - Operators
+//Type - constants
+let workDaysInAYear = 200
+let salaryPerMonth = 1000.0
+let myName = "Anderson"
+
+//Type inference
+var diasDeTrabalhoEmUmAno: Int = 200
+var salarioPorMes: Double = 1000.0
+var meuNome = "Anderson"
+
+//change variable
+salarioPorMes = 2000.0
